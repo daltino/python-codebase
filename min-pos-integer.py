08b8267573be522135b3a27b97b4e340f43c8f2d
Dalton Whyte <daltino06@yahoo.com>
2018-09-09 15:28:11 +0100

    Improved getMinPosInt algorithm to support corner cases with extreme valuess

--- min-pos-integer.py
+++ min-pos-integer.py
@@ -1,13 +1,16 @@
 # A simple Python function to derive the minimum positive
 # integer that is not present in an array and is greater than 
 def getMinPosInt(array):
- 	notFound = True
  	candidate = 1
- 	while notFound:
- 		if candidate not in array:
- 			notFound = False
- 		else:
- 			candidate = candidate + 1
- 	return candidate
+ 	for i in set(array):
+		if candidate < len(array) and candidate != i:
+			return candidate
+		elif candidate == len(array):
+			return candidate + 1
+		else:
+			candidate = candidate + 1
 
+print(getMinPosInt(range(1,100000)))
 print(getMinPosInt(range(1,50000)))
+print(getMinPosInt([1,3000,10000,20000,40000]))
+print(getMinPosInt([1,3,5]))
